b48b584b0c4bc9f5953f14b03ec96c3338dc4020
y992 <769025081@qq.com>
2017-06-08 06:33:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div class="top">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      </el-tabs>
+    </div>
+    <div class="left">
+      <el-col :span="8">
+        <el-menu default-active="3" router class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item-group router>
+              <template slot="title">分组一</template>
+              <el-menu-item index="/hello">选项1</el-menu-item>
+              <el-menu-item index="/1111">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="1"><i class="el-icon-menu"></i>导航二</el-menu-item>
+          <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+        </el-menu>
+      </el-col>
+    </div>
+    <div class="right">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    func() {
+      console.log('tanjinc')
+      alert('hello')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test1.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      test 1 !!!
+    </div>
+</template>
+<style>
+
+</style>
+<script type="text/ecmascript-6">
+
+    export default{
+
+    }
+</script>
